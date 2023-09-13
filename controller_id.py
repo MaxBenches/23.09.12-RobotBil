371c77e2d47dbb8b5346566ade913344f40ba93b
MaxBenches <113772275+MaxBenches@users.noreply.github.com>
2023-09-13 17:23:28 +0200

    Added start menu and multiple path for the different challenges

---
+++ controller_id.py
@@ -0,0 +1,16 @@
+import pygame
+
+# This code identifies controller ID's for buttons and axes
+
+pygame.joystick.init()
+controllers = [pygame.joystick.Joystick(x) for x in range(pygame.joystick.get_count())]
+pygame.init()
+
+while True:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pass
+        if event.type == pygame.JOYBUTTONDOWN:
+            print(event)
+        if event.type == pygame.JOYAXISMOTION:
+            print(event)
